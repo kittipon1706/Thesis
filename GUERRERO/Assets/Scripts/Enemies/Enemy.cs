ce3c7c181b2771aab536c35d77baf135ee028a87
kittipon1706 <kittiponkong0604@gmail.com>
2022-05-29 20:49:04 +0700

    Enemy+Behavior+OOP

---
+++ GUERRERO/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using BehaviorDesigner.Runtime;
+public class Enemy<T> where T : Enemy
+{
+    public GameObject GameObject;
+    public T ScriptComponant;
+
+    public Enemy(string name)
+    {
+        GameObject = new GameObject(name);
+        ScriptComponant = GameObject.AddComponent<T>();
+    }
+}
+
+public abstract class Enemy : MonoBehaviour
+{
+    public Rigidbody Body;
+    public NavMeshAgent navmesh;
+    public BehaviorTree BE;
+    public Animator Animator;
+    public CapsuleCollider Collider;
+    public GameObject Model;
+    public int Speed;
+    //Waypoint
+
+    //protected abstract void AttackPattern();
+
+    private void Awake()
+    {
+        //Add common componants
+        Body = gameObject.AddComponent<Rigidbody>();
+        Collider = gameObject.AddComponent<CapsuleCollider>();
+        navmesh = gameObject.AddComponent<NavMeshAgent>();
+        BE = gameObject.AddComponent<BehaviorTree>();
+        //Set common Model
+        Body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        Animator = gameObject.AddComponent<Animator>();
+
+        gameObject.tag = "Enemy";
+        gameObject.layer = LayerMask.NameToLayer("Enemies");
+    }
+
+    public virtual void Initialize(int speed,string Model_Name,Vector3 position,Vector3 scale, string ani,string avatar,string BE_name)
+    {
+        Speed = speed;
+        Model = Instantiate(Resources.Load<GameObject>("Art/3D/Enemies/Goblin/" + Model_Name), transform.position, Quaternion.identity);
+        Model.transform.localScale = scale;
+        Model.transform.SetParent(gameObject.transform);
+        gameObject.transform.position = position;
+        GetComponent<BehaviorTree>().ExternalBehavior = Resources.Load<ExternalBehavior>("Art/3D/Enemies/Goblin/" + BE_name);
+        GetComponent<Animator>().runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>("Art/3D/Enemies/Goblin/" + ani);
+        GetComponent<Animator>().avatar = Resources.Load<Avatar>("Art/3D/Enemies/Goblin/" + avatar);
+    }
+}
+
